27170d8e39195892b333603b3610ea487fc6513a
Lance Hammond Jr <lance.hammondjr@gmail.com>
2020-03-26 21:24:40 -0500

    bDatatypesH

---
+++ 01week/datatypes.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//Write a JavaScript program to display the current day and time.
+let now = Date();
+console.log("the current date and time", now);
+
+//Write a JavaScript program to convert a number to a string.
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("The type of theSring varaible is", (typeof theString))
+console.log("The type of theNumber varaible is", (typeof theNumber))
+console.log("The syring is");
+console.log("The number is", theNumber);
+
+
+//Write a JavaScript program to convert a string to the number.
+const theOtherString = "11";
+const theOtherNumber = (theOtherString, 10);
+console.log("The otherString type is", (typeof theOtherString));
+console.log("The otherNumber type is", (typeof theOtherNumber));
