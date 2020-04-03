1532a01f54bdfa30a27ea9d3251f123440211084
Lance Hammond Jr <lance.hammondjr@gmail.com>
2020-04-02 20:15:49 -0500

    rockPaperScissors2

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -21,31 +21,28 @@ const rockPaperScissors = (hand1, hand2) => {
   // Write code here
   // Use the unit test to see what is expected
 
-  if (hand1 === hand2){
-    if (hand1 === "rock" && hand2 === "paper"){
-      return "Hand two wins!";
-    }
-    if (hand1 === "paper" && hand2 === "scissors"){
-      return "Hand two wins!";
-    }  
-    if (hand1 === "scissors" && hand2 === "rock"){
-      return "Hand two wins!";
-    }
-    if (hand1 === "paper" && hand2 === "rock"){
-      return "Hand one wins!";
-    }  
-    if (hand1 === "rock" && hand2 === "scissors"){
-      return "Hand one wins!";
-    }  
-    if (hand1 === "scissors" && hand2 === "paper"){
-      return "Hand one wins!";
-    }
-    else{
-      return "It's a tie!";
-    
-    }
+if (hand1 === "rock" && hand2 === "paper"){
+    return "Hand two wins!";
   }
-
+else if (hand1 === "paper" && hand2 === "scissors"){
+    return "Hand two wins!";
+  }  
+else if (hand1 === "scissors" && hand2 === "rock"){
+    return "Hand two wins!";
+  }
+else if (hand1 === "paper" && hand2 === "rock"){
+    return "Hand one wins!";
+  }  
+else if (hand1 === "rock" && hand2 === "scissors"){
+    return "Hand one wins!";
+  }  
+else if (hand1 === "scissors" && hand2 === "paper"){
+    return "Hand one wins!";
+  }
+else (hand1 === hand2);{
+    return "It's a tie!";
+  }
+}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
